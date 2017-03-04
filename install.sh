7163df20a856e1fbf77b13b164835838f285ddc1
Dan Serban <dserban01@yahoo.com>
2017-03-05 01:50:11 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,12 +12,15 @@ echo 'Extracting JDK ...'
 tar -xf /opt/jdk.tgz -C /opt
 rm /opt/jdk.tgz
 mv /opt/jdk1.8.0_102 /opt/jdk
+mkdir /jars
 echo 'Downloading Spark-Cassandra Connector ...'
-wget -qO /opt/docker/lib/spark-cassandra-connector.jar \
+wget -qO /jars/spark-cassandra-connector.jar \
          https://repo1.maven.org/maven2/com/datastax/spark/spark-cassandra-connector_2.11/2.0.0-RC1/spark-cassandra-connector_2.11-2.0.0-RC1.jar
-echo 'Downloading SparkNotebooks ...'
+echo 'Downloading jsr166e ...'
+wget -qO /jars/jsr166e.jar \
+         https://repo1.maven.org/maven2/com/twitter/jsr166e/1.1.0/jsr166e-1.1.0.jar
+echo 'Downloading DockerSparkNotebook/SparkDatasets ...'
 cd /opt
 git clone https://github.com/dserban/DockerSparkNotebook
 git clone https://github.com/dserban/SparkDatasets
 echo 'Building container, this may take a while ...'
-
