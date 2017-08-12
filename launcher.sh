f665940c715f20b1909dfcccf231fa0535786c60
Dan Serban <dserban01@yahoo.com>
2017-08-12 22:11:01 +0000

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,8 @@
 echo "35.157.141.95 dbserver" >> /etc/hosts
-cp /jars/*.jar /opt/docker/lib/
 cd /opt/docker/notebooks
 rm -rf *
 cp /opt/DockerSparkNotebook/notebooks/*.snb .
 cd /opt/docker
+export EXTRA_CLASSPATH=/jars/opennlp-tools.jar:/jars/spark-cassandra-connector.jar:/jars/graphframes.jar
 bin/spark-notebook -Dhttp.port=${SNB_PORT}
 
