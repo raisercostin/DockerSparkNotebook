caae9f06ae4a43c6d1fd505c617fa28f6ae82e5d
Dan Serban <dserban01@yahoo.com>
2017-08-15 16:35:01 +0000

    Revise notebooks

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,7 @@
-echo "35.157.141.95 dbserver" >> /etc/hosts
+echo "123.123.123.123 dbserver" >> /etc/hosts
 cd /opt/docker/notebooks
 rm -rf *
-cp /opt/DockerSparkNotebook/notebooks/*.snb .
+cp /opt/DockerSparkNotebook/notebooks/*.snb.ipynb .
 cd /opt/docker
 export EXTRA_CLASSPATH=/jars/opennlp-tools.jar:/jars/spark-cassandra-connector.jar:/jars/graphframes.jar
 bin/spark-notebook -Dhttp.port=${SNB_PORT}
